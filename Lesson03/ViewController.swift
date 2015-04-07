4391c4a654cef4a35725b89c0a4f8e617f461aab
imbenham <isaacbenham@Isaacs-MacBook-Pro.local>
2015-04-07 18:06:47 -0400

    2nd release
    changed some colors and modal dismissal

--- Lesson03/ViewController.swift
+++ Lesson03/ViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 
+let viewBackgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.9, alpha: 0.9)
+let secondaryColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1)
+let trimColor = UIColor(red: 0.75, green: 0.0, blue: 0.1, alpha: 0.9)
+
 class ViewController: UIViewController {
     
-    @IBOutlet weak var swipeAreaLabel: UILabel!
+   
     
     @IBOutlet weak var tvButton: UIButton!
     
@@ -18,53 +22,61 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     
-    let viewBackgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.9, alpha: 0.9)
-    let trimColor = UIColor(red: 0.7, green: 0.7, blue: 0.2, alpha: 1)
-
-    
-    @IBAction func showModalDialogue(sender: UISwipeGestureRecognizer) {
+   
+    @IBAction func presentModalViewController(sender: UISwipeGestureRecognizer!) {
         
-        sender.cancelsTouchesInView = false
-        
-        let swipePoint = sender.locationInView(self.view)
-        
-        if swipeAreaLabel.frame.contains(swipePoint) {
-            let mDC = ModalDialogController()
-            mDC.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
-            
-            
-            self.presentViewController(mDC, animated: true, completion: nil)
-        }
-        
-     
+        println("function called")
+        let mDC = ModalDialogController()
+        mDC.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+        mDC.viewController = self
+        self.presentViewController(mDC, animated: true, completion: nil)
     }
-    
+
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        swipePad.backgroundColor = trimColor
+        swipePad.backgroundColor = secondaryColor
         swipePad.textColor = viewBackgroundColor
-        swipePad.layer.cornerRadius = 5
+        let swipeLayer = swipePad.layer
+        swipeLayer.cornerRadius = 5
+        swipeLayer.borderColor = trimColor.CGColor
+        swipeLayer.borderWidth = 5
+        
         swipePad.clipsToBounds = true
         self.view.backgroundColor = viewBackgroundColor
-        tvButton.backgroundColor = trimColor
+        tvButton.backgroundColor = secondaryColor
         tvButton.setTitleColor(viewBackgroundColor, forState: .Normal)
-        tvButton.layer.cornerRadius = 5
+        let buttonLayer = tvButton.layer
+        buttonLayer.cornerRadius = 5
+        buttonLayer.borderWidth = 3
+        buttonLayer.borderColor = trimColor.CGColor
         tvButton.clipsToBounds = true
         tvButton.addTarget(self, action: "showTableViewController:", forControlEvents: .TouchUpInside)
-        titleLabel.textColor = trimColor
+        titleLabel.textColor = secondaryColor
+        let titleLayer = titleLabel.layer
+        titleLayer.backgroundColor = trimColor.CGColor
+        titleLayer.cornerRadius = 5
+        
         
     }
     
     func showTableViewController(sender: AnyObject!) {
         self.performSegueWithIdentifier("show", sender: self)
     }
+    
+    func dismissModal() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
 
 class ModalDialogController: UIViewController {
+    var viewController : ViewController? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let viewBackgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.9, alpha: 0.9)
         self.view.backgroundColor = viewBackgroundColor
         
         let image = UIImage(named: "SmallLucy")
@@ -73,9 +85,11 @@ class ModalDialogController: UIViewController {
         imageView.clipsToBounds = true
         
         let imageBorder = UIView()
-        let trimColor = UIColor(red: 0.7, green: 0.7, blue: 0.2, alpha: 1)
-        imageBorder.backgroundColor = trimColor
-        imageBorder.layer.cornerRadius = 5
+        imageBorder.backgroundColor = secondaryColor
+        let borderLayer = imageBorder.layer
+        borderLayer.cornerRadius = 5
+        //borderLayer.borderWidth = 3
+        //borderLayer.borderColor = trimColor.CGColor
         imageBorder.addSubview(imageView)
         self.view.addSubview(imageBorder)
         
@@ -83,8 +97,11 @@ class ModalDialogController: UIViewController {
         button.setTitle("Dismiss", forState: .Normal)
         button.setTitle("ByeBye!", forState: .Highlighted)
         button.setTitleColor(viewBackgroundColor, forState: .Normal)
-        button.backgroundColor = trimColor
-        button.layer.cornerRadius = 5
+        button.backgroundColor = secondaryColor
+        let buttonLayer = button.layer
+        buttonLayer.cornerRadius = 5
+        buttonLayer.borderWidth = 2
+        buttonLayer.borderColor = trimColor.CGColor
         button.addTarget(self, action: "dismissModal:", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(button)
         
@@ -93,8 +110,8 @@ class ModalDialogController: UIViewController {
         button.setTranslatesAutoresizingMaskIntoConstraints(false)
         
         
-        let borderWidth = NSLayoutConstraint(item: imageBorder, attribute: .Width, relatedBy: .Equal, toItem: imageView, attribute: .Width, multiplier: 1, constant: 8)
-        let borderHeight = NSLayoutConstraint(item: imageBorder, attribute: .Height, relatedBy: .Equal, toItem: imageView, attribute: .Height, multiplier: 1, constant: 8)
+        let borderWidth = NSLayoutConstraint(item: imageBorder, attribute: .Width, relatedBy: .Equal, toItem: imageView, attribute: .Width, multiplier: 1, constant: 12)
+        let borderHeight = NSLayoutConstraint(item: imageBorder, attribute: .Height, relatedBy: .Equal, toItem: imageView, attribute: .Height, multiplier: 1, constant: 12)
         let borderHorCenter = NSLayoutConstraint(item: imageBorder, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1, constant: 0)
         let borderVerCenter = NSLayoutConstraint(item: imageBorder, attribute: .CenterY, relatedBy: .Equal, toItem: self.view, attribute: .CenterY, multiplier: 1, constant: 0)
         
@@ -102,7 +119,7 @@ class ModalDialogController: UIViewController {
         let imageVerCenter = NSLayoutConstraint(item: imageView, attribute: .CenterY, relatedBy: .Equal, toItem: imageBorder, attribute: .CenterY, multiplier: 1, constant: 0)
         
         let buttonHorizontalCenter = NSLayoutConstraint(item: button, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1, constant: 0)
-        let buttonWidth = NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 55)
+        let buttonWidth = NSLayoutConstraint(item: button, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 60)
         let buttonHeight = NSLayoutConstraint(item: button, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 30)
         let buttonFloor = NSLayoutConstraint(item: button, attribute: .Top, relatedBy: .Equal, toItem: imageBorder, attribute: .Bottom, multiplier: 1, constant: 8)
         
@@ -112,8 +129,13 @@ class ModalDialogController: UIViewController {
         
     }
     
+    //let myDrawColorPattern
+    
+
+    
     func dismissModal(sender: AnyObject!){
-        self.presentingViewController!.dismissViewControllerAnimated(true, completion: nil)
+        self.viewController!.dismissModal()
+        
     }
 }
 
